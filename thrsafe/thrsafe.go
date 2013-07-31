414eff9f45c4bf8432eaac957238278fd15f4616
Michał Derkacz <michal@Lnet.pl>
2013-07-31 20:14:15 +0200

    Remove race in thrsafe pinger

--- thrsafe/thrsafe.go
+++ thrsafe/thrsafe.go
@@ -65,9 +65,6 @@ func (c *Conn) Clone() mysql.Conn {
 }
 
 func (c *Conn) pinger() {
-	c.stopPinger = make(chan struct{})
-	defer func() { c.stopPinger = nil }()
-
 	const to = 60 * time.Second
 	sleep := to
 	for {
@@ -91,6 +88,7 @@ func (c *Conn) Connect() error {
 	//log.Println("Connect")
 	c.lock()
 	defer c.unlock()
+	c.stopPinger = make(chan struct{})
 	go c.pinger()
 	return c.Conn.Connect()
 }
